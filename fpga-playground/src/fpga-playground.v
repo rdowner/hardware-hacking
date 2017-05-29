35210915bf21e1d1388b295fffbf0f8434977404
Richard Downer <richard@apache.org>
2017-05-29 21:20:11 +0100

    Add empty fpga-playground project

---
+++ fpga-playground/src/fpga-playground.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:17:18 05/29/2017 
+// Design Name: 
+// Module Name:    fpga-playground 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module fpga-playground(
+    );
+
+
+endmodule
